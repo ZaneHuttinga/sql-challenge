6bd260c03c75569fe881df7b8104bd0d28c5bca5
Zane Huttinga <ZaneHuttinga@gmail.com>
2024-08-28 17:22:55 -0500

    Finish editing queries.sql file

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,11 +1,11 @@
 --List the employee number, last name, first name, sex, and salary of each employee.
-SELECT employees.emp_no, employees.first_name, employees.last_name, employees.sex, salaries.salary
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 JOIN salaries
 ON (employees.emp_no = salaries.emp_no);
 
 --List the first name, last name, and hire date for the employees who were hired in 1986.
-SELECT employees.first_name, employees.last_name, employees.hire_date
+SELECT first_name, last_name, hire_date
 FROM employees
 WHERE RIGHT(employees.hire_date,4) = '1986';
 
@@ -26,9 +26,9 @@ JOIN departments
 ON (dept_emp.dept_no = departments.dept_no);
 
 --List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
-SELECT employees.first_name, employees.last_name, employees.sex
+SELECT first_name, last_name, sex
 FROM employees
-WHERE employees.first_name = 'Hercules' AND LEFT(employees.last_name,1) = 'B';
+WHERE first_name = 'Hercules' AND LEFT(last_name,1) = 'B';
 
 --List each employee in the Sales department, including their employee number, last name, and first name.
 SELECT employees.emp_no, employees.last_name, employees.first_name
@@ -49,3 +49,7 @@ ON (departments.dept_no = dept_emp.dept_no)
 WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name, COUNT(*)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(*) DESC;
